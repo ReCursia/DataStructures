91c1570cdba8c951d859e36a4ee3689b73a86159
ReCursia <saha2098@yandex.ru>
2020-04-05 03:18:14 +0300

    implementing progress binary tree

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -8,6 +8,28 @@ namespace DataStructures
     {
         private static void Main()
         {
+            //TestLinkedList();
+            TestBinaryTree();
+        }
+
+        private static void TestBinaryTree()
+        {
+            ISet<int> tree = new TreeSet<int>();
+            tree.Add(5);
+            tree.Add(6);
+            tree.Add(9);
+            tree.Add(1);
+            tree.Add(4);
+            tree.Add(8);
+            tree.Add(-15);
+            tree.Remove(1);
+            Console.Out.WriteLine(tree.Contains(1));
+            Console.Out.WriteLine(tree.Contains(9));
+            foreach (var elem in tree) Console.Out.WriteLine(elem);
+        }
+
+        private static void TestLinkedList()
+        {
             IList<int> list = new LinkedList<int>();
             list.AddFirst(5);
             list.AddFirst(6);
--- DataStructures/structures/TreeSet.cs
+++ DataStructures/structures/TreeSet.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DataStructures.structures.enumerators;
+using DataStructures.structures.models;
 
 namespace DataStructures.structures
 {
     public class TreeSet<T> : common.ISet<T> where T : IComparable<T>
     {
+        private const int Equal = 0;
+        private const int LessThan = -1;
+        private const int GreaterThan = 1;
+        private TreeNode<T> _root;
+
+        public TreeSet()
+        {
+            _root = null;
+        }
+
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
@@ -13,22 +25,101 @@ namespace DataStructures.structures
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new TreeSetEnumerator<T>(_root);
         }
 
-        public void Add(T data)
+        public bool Add(T data)
         {
-            throw new NotImplementedException();
+            if (Contains(data)) return false;
+
+            if (_root == null)
+                _root = new TreeNode<T>(data);
+            else
+                AddElem(data, _root);
+
+            return true;
         }
 
-        public void Contains(T data)
+        public bool Contains(T data)
         {
-            throw new NotImplementedException();
+            return FindElem(data, _root);
         }
 
-        public void Remove(T data)
+        public bool Remove(T data)
         {
-            throw new NotImplementedException();
+            if (!Contains(data)) return false;
+            _root = RemoveElem(data, _root);
+            return true;
+        }
+
+        private static TreeNode<T> RemoveElem(T data, TreeNode<T> node)
+        {
+            if (node == null) return null;
+            var compareTo = data.CompareTo(node.Data);
+            if (compareTo == LessThan)
+            {
+                node.Left = RemoveElem(data, node.Left);
+            }
+            else if (compareTo == GreaterThan)
+            {
+                node.Right = RemoveElem(data, node.Right);
+            }
+            else
+            {
+                if (node.Right == null) return node.Left;
+                if (node.Left == null) return node.Right;
+                var temp = node;
+                node = MinElem(temp.Right);
+                node.Right = DeleteMin(temp.Right);
+                node.Left = temp.Left;
+            }
+
+            return node;
+        }
+
+        private static TreeNode<T> DeleteMin(TreeNode<T> node)
+        {
+            if (node.Left == null) return node.Right;
+            node.Left = DeleteMin(node.Left);
+            return node;
+        }
+
+        private static TreeNode<T> MinElem(TreeNode<T> node)
+        {
+            return node.Left == null ? node : MinElem(node.Left);
+        }
+
+        private static bool FindElem(T data, TreeNode<T> node)
+        {
+            if (node == null)
+                return false;
+
+            var compareTo = data.CompareTo(node.Data);
+            return compareTo switch
+            {
+                Equal => true,
+                LessThan => FindElem(data, node.Left),
+                GreaterThan => FindElem(data, node.Right),
+                _ => false
+            };
+        }
+
+        private static void AddElem(T data, TreeNode<T> node)
+        {
+            if (data.CompareTo(node.Data) == LessThan)
+            {
+                if (node.Left != null)
+                    AddElem(data, node.Left);
+                else
+                    node.Left = new TreeNode<T>(data);
+            }
+            else
+            {
+                if (node.Right != null)
+                    AddElem(data, node.Right);
+                else
+                    node.Right = new TreeNode<T>(data);
+            }
         }
     }
 }
